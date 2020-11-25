b745eb19a5d26ebff225a027834d1223e43b33fa
王海军 <haijun.wang02@weimob.com>
2020-11-25 19:55:36 +0800

    传递机制

--- src/面向对象/day9/P18再谈方法/方法形参的传递机制4/ValueTransferTest1.java
+++ src/面向对象/day9/P18再谈方法/方法形参的传递机制4/ValueTransferTest1.java
@@ -16,7 +16,20 @@ public class ValueTransferTest1 {
 
         System.out.println("m = " + m + ", n = " + n);
         //交换两个变量的值操作
+//        int temp = m;
+//        m = n;
+//        n = temp;
+
+        ValueTransferTest1 test = new ValueTransferTest1();
+        test.swap(m , n);
+
+        System.out.println("m = " + m + ", n = " + n);
+    }
+
+    public void swap(int m , int n){
         int temp = m;
         m = n;
+        n = temp;
+
     }
 }
